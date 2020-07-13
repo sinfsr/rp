4cdb63caab3f43b6b54627367a5f85251972cc34
sinfsr <67871113+sinfsr@users.noreply.github.com>
2020-07-13 04:14:08 -1200

    Update herokubot.py

--- herokubot.py
+++ herokubot.py
@@ -83,9 +83,13 @@ def echo(bot, update):
         update.effective_message.reply_text("0")
         driver.switch_to_window(driver.window_handles[0])
         text = driver.find_element_by_tag_name("body").text
+        if sahm in text:
+            update.effective_message.reply_text("سهم در تکست است")          
         driver.switch_to_window(driver.window_handles[1])
         text1 = driver.find_element_by_tag_name("body").text
         update.effective_message.reply_text("1")
+        if sahm in text1 :
+            update.effective_message.reply_text("سهم در تکست1 است")   
     except:
         update.effective_message.reply_text("EROR")
            
