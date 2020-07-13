a82e7cf24f7bd5f056f07acde7b71c64937f916b
sinfsr <67871113+sinfsr@users.noreply.github.com>
2020-07-13 00:31:08 -1200

    Update herokubot.py

--- herokubot.py
+++ herokubot.py
@@ -1,7 +1,6 @@
 import logging
 import os
 from selenium import webdriver
-import clipboard
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
