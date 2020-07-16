93ba4c1c23c00d747e67868f0c78ac97dacc8b9e
sinfsr <67871113+sinfsr@users.noreply.github.com>
2020-07-15 21:43:34 -1200

    Update herokubot.py

--- herokubot.py
+++ herokubot.py
@@ -5,6 +5,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from pprint import pprint
+import requests
+from urllib.parse import urljoin
 
 
 ########
