0abfc9366060d3d7a251dabede6d46d0de7e1145
sinfsr <67871113+sinfsr@users.noreply.github.com>
2020-07-16 08:51:12 -1200

    Update herokubot.py

--- herokubot.py
+++ herokubot.py
@@ -20,94 +20,6 @@ chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 ########
 ########
-def python(bot, update):
-    update.effective_message.reply_text("let's go.")
-    while True:
-        update.effective_message.reply_text("while true started.")
-
-        try:
-            driver.get("http://www.google.com/") 
-            windows_before  = driver.current_window_handle
-            update.effective_message.reply_text("went DOODLE.")
-            api_token = "daf7ae63bb884392c4f050bf67d5800ebacb1fa4"
-            username = "sinfsr"
-            pythonanywhere_host = "www.pythonanywhere.com"
-
-            api_base = "https://{pythonanywhere_host}/api/v0/user/{username}/".format(
-                pythonanywhere_host=pythonanywhere_host,
-                username=username,
-            )
-            update.effective_message.reply_text("conected to P.A .")
-            def fill(ntab , txtname , fl):
-                driver.execute_script("window.open('http://www.tsetmc.com/')")  
-                driver.switch_to_window(driver.window_handles[ntab])
-                element10 = driver.find_element(By.XPATH , '/html/body/div[3]/div[2]/a[4]')
-                element10.click()
-                time.sleep(2)
-                element11 = driver.find_element(By.XPATH , '//*[@id="id1"]')
-                time.sleep(2)
-                element11.click()
-                time.sleep(2)
-                element12 = driver.find_element(By.XPATH , '/html/body/div[7]/div[3]/div[1]/div[32]')
-                time.sleep(2)
-                element12.click()  
-                time.sleep(1.9)
-                element13 = driver.find_element(By.XPATH , '//*[@id="id1"]')
-                time.sleep(2)
-                element13.click()
-                time.sleep(2)
-                element14 = driver.find_element(By.XPATH , '/html/body/div[7]/div[3]/div[1]/div[33]')
-                time.sleep(2)
-                element14.click()           
-                time.sleep(2)
-                element15 = driver.find_element(By.XPATH , '//*[@id="id1"]')
-                time.sleep(1.9)
-                element15.click()
-                time.sleep(2)
-                element16 = driver.find_element(By.XPATH , '/html/body/div[7]/div[3]/div[1]/div[34]')
-                time.sleep(2)
-                element16.click()  
-                time.sleep(1.9)
-                element1 = driver.find_element(By.XPATH , '/html/body/div[6]/div[1]/a[7]')
-                time.sleep(2)
-                element1.click()  
-                element2 = driver.find_element(By.XPATH , '//*[@id="FilterIndex"]/div[1]')
-                time.sleep(2)
-                element2.click()
-                elememt3 = driver.find_element(By.XPATH , '/html/body/div[7]/div[3]/div[1]/div[1]')
-                time.sleep(2)
-                elememt3.click()
-                elememt4 = driver.find_element(By.XPATH , '//*[@id="InputFilterCode"]')
-
-                time.sleep(2)
-                elememt4.send_keys(fl)
-                elememt5 = driver.find_element(By.XPATH , '//*[@id="FilterContent"]/div[1]')
-                time.sleep(2)
-                elememt5.click()
-                iii = driver.find_element_by_tag_name("body").text
-                resp = requests.post(
-                urljoin(api_base, "files/path/home/{username}/{txtname}".format(username=username , txtname=txtname)),
-                files={"content": iii },
-                headers={"Authorization": "Token {api_token}".format(api_token=api_token)}
-                )
-                update.effective_message.reply_text("done befor close.")
-                driver.switch_to_window(driver.window_handles[ntab])
-                driver.close()
-            fill(1 , "1.txt" , "abc")
-            update.effective_message.reply_text("1 done.")
-            fill(2 , "2.txt" , "efg")
-            update.effective_message.reply_text("2 done.")
-        except:
-            update.effective_message.reply_text("not done.")
-            try:
-                
-                driver.close()
-                update.effective_message.reply_text("driver closed.")
-            except:
-                update.effective_message.reply_text("driver was'nt oppen.")
-
-
-
 def check(bot, update):
     try:
         update.effective_message.reply_text("let us get started!")
@@ -387,7 +299,6 @@ if __name__ == "__main__":
     dp = updater.dispatcher
     # Add handlers
     dp.add_handler(CommandHandler('start', start))
-    dp.add_handler(CommandHandler('python', python))
     dp.add_handler(CommandHandler('run', run))
     dp.add_handler(CommandHandler('check', check))
     dp.add_handler(CommandHandler('run1', run1))
